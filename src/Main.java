ae4c16c035f68e5d80e20fb942ac2e3c4c585355
Rob Williams <robwilliams@Robs-MacBook-Pro.local>
2024-01-05 14:27:56 -0700

    added comments for better readability

--- src/Main.java
+++ src/Main.java
@@ -7,15 +7,20 @@ public class Main {
 
    public static void main(String[] args) {
 
+       JOptionPane.showMessageDialog(null, "Welcome to the baby weight converter!");
+
        String babyName = JOptionPane.showInputDialog("What is your baby's name?");
 
        double weight = Double.parseDouble(JOptionPane.showInputDialog("What is baby's weight in lbs? "));
 
+       // Get just the first two values of the double to represent the 1lbs of babies weight as an integer
        int lbWeight = (int) weight;
 
+       // Get just the values after the decimal point of weight variable to represent ounces
        String numberWeight = String.valueOf(weight);
        numberWeight = numberWeight.substring(numberWeight.indexOf("."));
 
+       // Convert numberWeight back to double type and multiply value by 16 to convert from lbs to oz as a double type
        double ozWeight = Double.parseDouble(numberWeight);
        ozWeight = ozWeight * 16;
        JOptionPane.showMessageDialog(null, babyName + "'s weight in lbs and oz is " + lbWeight + "lbs " + ozWeight + "oz.");
