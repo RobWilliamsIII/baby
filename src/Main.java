b546b9e7826f663ffe329b8434327e27470381f1
Rob Williams <robwilliams@Robs-MacBook-Pro.local>
2024-01-06 09:55:48 -0700

    changed from scanner to JOptiopane

--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,35 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-import java.util.Scanner;
+
 import javax.swing.JOptionPane;
 
 public class Main {
 
    public static void main(String[] args) {
 
-       JOptionPane.showMessageDialog(null, "Welcome to the baby weight converter!");
+       JOptionPane.showMessageDialog(null, "Welcome to the baby weight converter!", "Welcome", JOptionPane.INFORMATION_MESSAGE );
 
-       String babyName = JOptionPane.showInputDialog("What is your baby's name?");
+       String babyName = JOptionPane.showInputDialog(null, "What is your baby's name?", "Baby Name", JOptionPane.QUESTION_MESSAGE);
 
-       double weight = Double.parseDouble(JOptionPane.showInputDialog("What is baby's weight in lbs? "));
+       while (babyName.isBlank()) {
+           JOptionPane.showMessageDialog(null, "A name must be entered", "No Input", JOptionPane.ERROR_MESSAGE);
+           babyName = JOptionPane.showInputDialog(null, "What is your baby's name?", "Baby Name", JOptionPane.QUESTION_MESSAGE);
+            }
 
-       // Get just the first two values of the double to represent the 1lbs of babies weight as an integer
-       int lbWeight = (int) weight;
+           double weight = Double.parseDouble(JOptionPane.showInputDialog("What is the baby's weight in lbs?"));
+               // Get just the first two values of the double to represent the lbs of baby's weight as an integer
 
-       // Get just the values after the decimal point of weight variable to represent ounces
-       String numberWeight = String.valueOf(weight);
-       numberWeight = numberWeight.substring(numberWeight.indexOf("."));
+               int lbWeight = (int) weight;
 
-       // Convert numberWeight back to double type and multiply value by 16 to convert from lbs to oz as a double type
-       double ozWeight = Double.parseDouble(numberWeight);
-       ozWeight = ozWeight * 16;
-       JOptionPane.showMessageDialog(null, babyName + "'s weight in lbs and oz is " + lbWeight + "lbs " + ozWeight + "oz.");
+               // Get just the values after the decimal point of weight variable to represent ounces
+               String numberWeight = String.valueOf(weight);
+               numberWeight = numberWeight.substring(numberWeight.indexOf("."));
 
-    }
+               // Convert numberWeight back to double type and multiply value by 16 to convert from lbs to oz as a double type
+               double ozWeight = Double.parseDouble(numberWeight);
+               ozWeight = ozWeight * 16;
+               JOptionPane.showMessageDialog(null, babyName + "'s weight in lbs and oz is " + lbWeight + "lbs " + ozWeight + "oz.");
+
+
+       }
 }
